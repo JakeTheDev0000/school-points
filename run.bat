6afa70f838ba4a1484a809a22f18fd35da3f0f6d
Jacob Obrien <jacobrien64@gmail.com>
2022-05-13 00:19:33 -0700

    last known version 0.3.1

--- run.bat
+++ run.bat
@@ -14,12 +14,12 @@ git push
 echo.
 echo.
 
-pip install -r requirements.txt %1
+pip install -r requirements.txt 
 
 echo.
 echo.
 
-python3 src\main.py 
+python3 src\main.py %1
 
 echo.
 echo cleaning up...
